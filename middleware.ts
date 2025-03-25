61eebb3ed322317ca7b00ff107588993cf5bbb49
NoarMiller <johnnydpx0@gmail.com>
2025-03-25 18:05:27 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,22 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse, NextRequest } from "next/server";
+import { getToken } from 'next-auth/jwt';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
   const isSignInPage = req.nextUrl.pathname === '/admin';
 
-  if (!token || token.username !== "llccxmiller") {
+  if (!token || !isAuthorizedUser(token)) {
     if (!isSignInPage) {
       return NextResponse.redirect(new URL('/', req.url));
     }
   }
 
   return NextResponse.next();
+}
+
+function isAuthorizedUser(token: { username: string }) {
+  return token && token.username === 'llccxmiller';
 }
 
 export const config = {
